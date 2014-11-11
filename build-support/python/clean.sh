0cad9644e9863962a46fb2bab31b492b58767988
Nick Howard <nhoward@twitter.com>
2014-11-11 13:51:36 -0700

    rm PANTS_VENV on clean.sh if it is set

--- build-support/python/clean.sh
+++ build-support/python/clean.sh
@@ -4,6 +4,9 @@ PANTS_BASE=$(dirname $0)/../..
 rm -rf ${HOME}/.pex
 rm -rf ${PANTS_BASE}/build-support/pants_deps.venv
 rm -rf ${PANTS_BASE}/build-support/pants_dev_deps.venv
+if [ ! -z ${PANTS_VENV} ]; then
+  rm -rf ${PANTS_VENV}
+fi
 rm -f  ${PANTS_BASE}/pants.pex
 rm -rf ${PANTS_BASE}/.pants.d
 find ${PANTS_BASE} -name '*.pyc' | xargs rm -f
